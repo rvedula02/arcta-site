041955c25d29f2010fadc49eee73b6596960abb8
Arjun Hegde <arjun.hegde02@gmail.com>
2025-05-05 00:51:12 -0400

    help

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google';
 import "./globals.css";
-import Navbar from "@/components/layout/Navbar";
-import Footer from "@/components/layout/Footer";
+import Navbar from "../components/layout/Navbar";
+import Footer from "../components/layout/Footer";
 import AuthProvider from "./providers";
 
 const font = Inter({ subsets: ['latin'] });
