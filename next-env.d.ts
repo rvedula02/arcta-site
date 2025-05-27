7586523455036a2751d08f18e98f479283880a80
rvedula02 <rv299@cornell.edu>
2025-05-27 10:35:53 -0400

    trying to fix deploy

---
+++ next-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file is required by Next.js and should not be edited. 
